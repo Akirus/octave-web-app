1aa3feb6b1838a53ee3b1442f6214497a4aab845
Alex Turchynovich <Akirus@inbox.ru>
2017-11-18 17:35:44 +0300

    Initial commit

---
+++ src/main/java/me/alextur/octave/OctaveApplication.java
@@ -0,0 +1,12 @@
+package me.alextur.octave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctaveApplication.class, args);
+	}
+}
